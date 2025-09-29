7b5e5a08ff3013cf49026827fb365273a0969350
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-09-29 13:28:41 +0100

    comment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@ import { resolve } from '$app/paths';
   const {children} = $props()
 </script>
 
+<!-- This causes an error on prerendering but it worked in version 2.43.4 -->
 <a href={resolve('/about')}>about</a>
 
 <!-- This works actually -->
